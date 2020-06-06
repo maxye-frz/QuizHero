7dc6a809ac5478ca0af5ff98b363ff6e862b9f41
yaozixuan <yaozixuan0411@gmail.com>
2020-06-06 00:40:17 -0400

    small style fix

--- code/Front End with React/src/components/default_theme/markdown-theme.js
+++ code/Front End with React/src/components/default_theme/markdown-theme.js
@@ -4,21 +4,30 @@ export default
 
     section {
       background-color: #FFFAFA;
-      height: 300px;
+      height: calc(25vw);
       width: calc(50vw);
       color: #000;
-      font-size: 12px;
-      padding: 13px;
+      font-size: 15px;
+      padding: 40px;
+      border: 1px solid rgba(15, 15, 15, 0.3);
     }
 
     h1 {
+      font-size: 30px;
       color: #FF1493;
       text-align: center;
+      margin: 30px;
     }
     
     h2 {
+      font-size: 20px;
       color: #FF1493;
       text-align: center;
-      margin: 0;
+      margin: 30px;
+    }
+    
+    p {
+      margin: 30px;
+      line-height: 1.5;
     }
     `
